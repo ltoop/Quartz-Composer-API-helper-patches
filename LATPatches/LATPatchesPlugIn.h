e99f07b653c5e96205fa8ae2d9c8f8175441a757
Luke Toop <luke@luketoop.com>
2013-08-25 18:22:38 +1000

    Initial Commit

---
+++ LATPatches/LATPatchesPlugIn.h
@@ -0,0 +1,17 @@
+//
+//  LATPatchesPlugIn.h
+//  LATPatches
+//
+//  Created by Luke Toop on 25/08/13.
+//  Copyright (c) 2013 LukeToop.com. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface LATPatchesPlugIn : QCPlugIn
+
+// Declare here the properties to be used as input and output ports for the plug-in e.g.
+//@property double inputFoo;
+//@property (copy) NSString* outputBar;
+
+@end
